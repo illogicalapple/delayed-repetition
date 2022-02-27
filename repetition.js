0a86c53f20a3ed6f689c7b5b98afd047be5858ab
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-02-27 11:27:22 -0600

    Update repetition.js

--- repetition.js
+++ repetition.js
@@ -2,26 +2,29 @@ class Thing {
 	constructor(array, weight) {
 		this.array = array;
 		this.weight = weight;
-		this.picking = array.map(e => {
+		this.picking = array.map(e => ({
 			value: e,
 			amount: 1
-		});
+		}));
 	}
 	random(change) {
-		let result = Math.floor(Math.random() * this.picking.length);
 		let newArray = [];
+		this.picking.forEach(e => {
+			newArray = newArray.concat((new Array(e.amount)).fill(e.value))
+		});
+		let result = Math.floor(Math.random() * newArray.length);
 		if(change) {
-			this.picking.map((e, i) => {
+			this.picking.map((e, i) => ({
 				value: e.value,
 				amount: e.amount + (this.weight * (result != i))
-			});
+			}));
 		}
-		return picking.map(e => )[result];
+		return newArray[result];
 	}
 	clear() {
-		this.picking.map(e => {
+		this.picking.map(e => ({
 			value: e.value,
 			amount: 1
-		});
+		}));
 	}
 }
